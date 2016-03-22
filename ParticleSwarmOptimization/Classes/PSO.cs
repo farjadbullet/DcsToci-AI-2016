ab06ea0a64ae0846523b34763996f60fc66e0723
SyedFarjadZiaZaidi <farjad_bullet@rocketmail.com>
2016-03-22 07:23:00 +0500

    Particle Swarm Optimization Implementation

---
+++ ParticleSwarmOptimization/Classes/PSO.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSwarmOptimization.Classes
+{
+    public class GraphMapper
+    {
+        public string Category { get; set; }
+        public double Value { get; set; }
+    }
+    public class SeriesMapper
+    {
+        public string Category { get; set; }
+        public List<GraphMapper> GraphDataSource { get; set; }
+    }
+    public class Vector<T>
+    {
+        public T ValueX { get; set; }
+        public T ValueY { get; set; }
+        public T FitnessValue => ((dynamic)ValueX * ValueX) + ((dynamic)ValueY * ValueY);
+
+        public Vector()
+        {
+            ValueX = (dynamic)0;
+            ValueY = (dynamic)0;
+        }
+        public Vector(dynamic a, dynamic b)
+        {
+            ValueX = a;
+            ValueY = b;
+        }
+
+        public static Vector<T> operator +(Vector<T> v1, Vector<T> v2)
+        {
+            return new Vector<T>(
+                (dynamic)v1.ValueX + v2.ValueX,
+                (dynamic)v1.ValueY + v2.ValueY);
+        }
+        public static Vector<T> operator +(Vector<T> v1, dynamic value)
+        {
+            return new Vector<T>(
+                (dynamic)v1.ValueX + value,
+                (dynamic)v1.ValueY + value);
+        }
+        public static Vector<T> operator -(Vector<T> v1, dynamic value)
+        {
+            return new Vector<T>(
+                (dynamic)v1.ValueX - value,
+                (dynamic)v1.ValueY - value);
+        }
+        public static Vector<T> operator -(Vector<T> v1, Vector<T> v2)
+        {
+            return new Vector<T>(
+                (dynamic)v1.ValueX - v2.ValueX,
+                (dynamic)v1.ValueY - v2.ValueY);
+        }
+
+        public static Vector<T> operator *(Vector<T> v1, dynamic value)
+        {
+            return new Vector<T>(
+                (dynamic)v1.ValueX * value,
+                (dynamic)v1.ValueY * value);
+        }
+        public static Vector<T> operator *(dynamic value, Vector<T> v1)
+        {
+            return new Vector<T>(
+                (dynamic)v1.ValueX * value,
+                (dynamic)v1.ValueY * value);
+        }
+
+    }
+    public class Chromosome
+    {
+        private double _valueX;
+        public double ValueX
+        {
+            get { return _valueX; }
+            set
+            {
+                if (value > 2)
+                {
+                    _valueX = 2;
+                }
+                else if (value < -2)
+                {
+                    _valueX = -2;
+                }
+                else
+                {
+                    _valueX = value;
+                }
+            }
+        }
+
+        private double _valueY;
+
+        public double ValueY
+        {
+            get { return _valueY; }
+            set
+            {
+                if (value > 5)
+                {
+                    _valueY = 5;
+                }
+                else if (value < -5)
+                {
+                    _valueY = -5;
+                }
+                else
+                {
+                    _valueY = value;
+                }
+            }
+        }
+        public double FitnessValue => (ValueX * ValueX) + (ValueY * ValueY);
+
+        public Vector<double> Velocity { get; set; }
+        public Vector<double> LocalBest { get; set; }
+        public Vector<double> GlobalBest { get; set; }
+
+        public Chromosome()
+        {
+            Velocity = new Vector<double>(ValueX, ValueY);
+            LocalBest = new Vector<double>(ValueX, ValueY);
+            GlobalBest = new Vector<double>(ValueX, ValueY);
+        }
+        public void CalculateVelocity()
+        {
+            var q = NextDouble(new Random(), 0, 1);
+            var x = 2*NextDouble(new Random(), 0, 1);
+            var y = (LocalBest - new Vector<double>(ValueX, ValueY));
+            var z = (2*NextDouble(new Random(), 0, 1)*(GlobalBest - new Vector<double>(ValueX, ValueY)));
+            Velocity = Velocity +
+                   (2 * NextDouble(new Random(), 0, 1) * (LocalBest - new Vector<double>(ValueX, ValueY)))
+                   + (2 * NextDouble(new Random(), 0, 1) * (GlobalBest - new Vector<double>(ValueX, ValueY)));
+        }
+
+        public void UpdatePosition()
+        {
+            ValueX = Velocity.ValueX;
+            ValueY = Velocity.ValueY;
+        }
+
+        public static double NextDouble(Random rng, double min, double max)
+        {
+            return min + (rng.NextDouble() * (max - min));
+        }
+    }
+
+    public class Population
+    {
+        #region Public Properties
+        public List<Chromosome> Chromosomes { get; set; }
+        public Vector<double> GlobalBest { get; set; }
+        public Vector<double> LocalBest { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public Population(Random random)
+        {
+            Chromosomes = new List<Chromosome>();
+            Enumerable.Range(1, 50).ToList().ForEach(m =>
+            {
+                var chromosome = new Chromosome
+                {
+                    ValueX = Chromosome.NextDouble(random, -2, 2),
+                    ValueY = Chromosome.NextDouble(random, -5, 5),
+                };
+                chromosome.GlobalBest = new Vector<double>(chromosome.ValueX, chromosome.ValueY);
+                chromosome.LocalBest = new Vector<double>(chromosome.ValueX, chromosome.ValueY);
+                Chromosomes.Add(chromosome);
+            });
+            var best = Chromosomes.FirstOrDefault(m => m.FitnessValue == Chromosomes.Max(x => x.FitnessValue));
+
+            if (best != null)
+            {
+                GlobalBest = best.GlobalBest;
+                LocalBest = best.LocalBest;
+            }
+        }
+
+        public Population(List<Chromosome> chromosomes)
+        {
+            Chromosomes = chromosomes;
+        }
+        #endregion
+
+        #region Calculation Helpers
+
+
+        #endregion
+
+        #region Ordering Helpers
+        public void OrderChromosomes()
+        {
+            Chromosomes = Chromosomes.OrderBy(m => m.FitnessValue).ToList();
+        }
+        public void OrderChromosomesDescending()
+        {
+            Chromosomes = Chromosomes.OrderByDescending(m => m.FitnessValue).ToList();
+        }
+        #endregion
+
+        #region Genetic Algorithm Operations Helpers
+
+        #endregion
+    }
+}
---
+++ ParticleSwarmOptimization/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ParticleSwarmOptimization.Classes;
+using Telerik.Charting;
+using Telerik.WinControls.UI;
+
+namespace ParticleSwarmOptimization
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Random random = new Random();
+            // Initial Population
+            Population initialPopulation = new Population(random);
+
+            // List to store best of every iteration
+            var bestResult = new List<double>();
+
+            // List to store average of every iteration
+            var averageResult = new List<double>();
+            List<GraphMapper> globalBestOfIteration = new List<GraphMapper>();
+            List<GraphMapper> localBestOfIteration = new List<GraphMapper>();
+            List<GraphMapper> averageOfIteration = new List<GraphMapper>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                List<GraphMapper> populationFitnessValue = new List<GraphMapper>();
+                List<GraphMapper> populationGFitnessValue = new List<GraphMapper>();
+
+                foreach (Chromosome chromosome in initialPopulation.Chromosomes)
+                {
+                    if (chromosome.FitnessValue > initialPopulation.LocalBest.FitnessValue)
+                    {
+                        initialPopulation.LocalBest.ValueX = chromosome.ValueX;
+                        initialPopulation.LocalBest.ValueY = chromosome.ValueY;
+                    }
+
+
+                    if (initialPopulation.LocalBest.FitnessValue > initialPopulation.GlobalBest.FitnessValue)
+                    {
+                        initialPopulation.GlobalBest = initialPopulation.LocalBest;
+                    }
+                }
+                foreach (Chromosome chromosome in initialPopulation.Chromosomes)
+                {
+                    chromosome.CalculateVelocity();
+                    chromosome.UpdatePosition();
+
+                    populationFitnessValue.Add(new GraphMapper
+                    {
+                        Category = "C " + (initialPopulation.Chromosomes.IndexOf(chromosome) + 1).ToString(),
+                        Value = chromosome.FitnessValue,
+                    });
+
+                    populationGFitnessValue.Add(new GraphMapper
+                    {
+                        Category = "C " + (initialPopulation.Chromosomes.IndexOf(chromosome) + 1).ToString(),
+                        Value = chromosome.GlobalBest.FitnessValue,
+                    });
+                }
+
+                List<SeriesMapper> listOfGraphMappers = new List<SeriesMapper>
+                {
+                    new SeriesMapper
+                    {
+                        Category = "Position Fitness Value",
+                        GraphDataSource = populationFitnessValue,
+                    },
+                    new SeriesMapper
+                    {
+                        Category = "Global Best Fitness Value",
+                        GraphDataSource = populationGFitnessValue,
+                    },
+                };
+
+                #region Add Charts dynamically to Form
+
+                CartesianArea cartesianArea = new CartesianArea();
+                int heightGraph = 0;
+                if (i > 0)
+                {
+                    heightGraph = this.Controls[i - 1].Height + this.Controls[i - 1].Location.Y;
+                }
+
+                RadChartView radChart = new RadChartView
+                {
+                    AreaDesign = cartesianArea,
+                    Location = new Point(0, heightGraph),
+                    Name = "radChartView1",
+                    ShowGrid = true,
+                    Size = new Size(1300, 700),
+                    TabIndex = 0,
+                    Text = "radChartView1",
+                    Title = "Iteration " + (i + 1).ToString(),
+                    ShowTitle = true,
+                    ShowLegend = true,
+                };
+                foreach (SeriesMapper graphMappers in listOfGraphMappers)
+                {
+                    LineSeries series = new LineSeries
+                    {
+                        LegendTitle = graphMappers.Category,
+                        PointSize = new SizeF(10, 10),
+                        BorderWidth = 2,
+                        CategoryMember = "Category",
+                        ValueMember = "Value",
+                        DataSource = graphMappers.GraphDataSource,
+                        ShowLabels = true,
+                        CombineMode = ChartSeriesCombineMode.None
+                    };
+                    radChart.Series.Add(series);
+                    radChart.ShowSmartLabels = true;
+
+                    ((CartesianArea)radChart.View.Area).ShowGrid = true;
+                }
+                this.Controls.Add(radChart);
+                radChart.ExportToImage(@"C:\Users\Farjad\Desktop\TOCI2\" + radChart.Title + ".png", radChart.Size);
+                #endregion
+
+                globalBestOfIteration.Add(new GraphMapper
+                {
+                    Category = "G " + (i + 1).ToString(),
+                    Value = initialPopulation.LocalBest.FitnessValue,
+                    //Value = initialPopulation.Chromosomes.FirstOrDefault(m => m.FitnessValue == initialPopulation.Chromosomes.Max(w => w.FitnessValue)).FitnessValue,
+                });
+                localBestOfIteration.Add(new GraphMapper
+                {
+                    Category = "G " + (i + 1).ToString(),
+                    Value = initialPopulation.Chromosomes.Max(m => m.FitnessValue),
+                });
+                averageOfIteration.Add(new GraphMapper
+                {
+                    Category = "G " + (i + 1).ToString(),
+                    Value = initialPopulation.Chromosomes.Average(m => m.FitnessValue),
+                });
+
+            }
+
+            var x = 0;
+            List<SeriesMapper> listOfSeries = new List<SeriesMapper>
+                {
+                    new SeriesMapper
+                    {
+                        Category = "Global Best of Iteration",
+                        GraphDataSource = globalBestOfIteration,
+                    },
+                    new SeriesMapper
+                    {
+                        Category = "Local Best of Iteration",
+                        GraphDataSource = localBestOfIteration,
+                    },
+                     new SeriesMapper
+                    {
+                        Category = "Average Best of Iteration",
+                        GraphDataSource = averageOfIteration,
+                    },
+                };
+
+            #region Add Charts dynamically to Form
+
+            CartesianArea cartesianArea1 = new CartesianArea();
+            int height = 0;
+            height = this.Controls[Controls.Count - 1].Height + this.Controls[Controls.Count - 1].Location.Y;
+
+            RadChartView chart = new RadChartView
+            {
+                AreaDesign = cartesianArea1,
+                Location = new Point(0, height),
+                Name = "radChartView1",
+                ShowGrid = true,
+                Size = new Size(1300, 700),
+                TabIndex = 0,
+                Text = "radChartView1",
+                Title = "Summary Graph",
+                ShowTitle = true,
+                ShowLegend = true,
+            };
+            foreach (SeriesMapper graphMappers in listOfSeries)
+            {
+                LineSeries series = new LineSeries
+                {
+                    LegendTitle = graphMappers.Category,
+                    PointSize = new SizeF(10, 10),
+                    BorderWidth = 2,
+                    CategoryMember = "Category",
+                    ValueMember = "Value",
+                    DataSource = graphMappers.GraphDataSource,
+                    ShowLabels = true,
+                    CombineMode = ChartSeriesCombineMode.None
+                };
+                chart.Series.Add(series);
+                chart.ShowSmartLabels = true;
+
+                ((CartesianArea)chart.View.Area).ShowGrid = true;
+            }
+            this.Controls.Add(chart);
+            chart.ExportToImage(@"C:\Users\Farjad\Desktop\TOCI2\" + chart.Title + ".png", chart.Size);
+            #endregion
+
+
+        }
+    }
+}
